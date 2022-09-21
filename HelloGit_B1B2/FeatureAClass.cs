366459a49455af834714f4a4f15f1be7f73469a2
Bernhard Wallisch <walliscb@technikum-wien.at>
2022-09-21 12:08:26 +0200

    add Feature A

---
+++ HelloGit_B1B2/FeatureAClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGit_B1B2
+{
+    internal class FeatureAClass
+    {
+    }
+}
