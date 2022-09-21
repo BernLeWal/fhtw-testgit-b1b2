a890c64abe7156a2bae6e407618b50bbca349001
Bernhard Wallisch <walliscb@technikum-wien.at>
2022-09-21 12:12:37 +0200

    add feature A implementation

--- HelloGit_B1B2/FeatureAClass.cs
+++ HelloGit_B1B2/FeatureAClass.cs
@@ -9,5 +9,10 @@ namespace HelloGit_B1B2
     internal class FeatureAClass
     {
         private int _aSecretNumber = 4711;
+
+        public void DoFeatureA()
+        {
+            Console.WriteLine("This is Feature A");
+        }
     }
 }
